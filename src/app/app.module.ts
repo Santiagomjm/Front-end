73c6dca2898c7959f13077d83557d784ed11b020
Santiago Moreira <santiagomjm@outlook.com>
2022-12-24 20:36:24 -0300

    boton, correciones boostrap, vistas celular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { BodyComponent } from './components/body/body.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoComponent,
-    SocialComponent,
     BannerComponent,
     BodyComponent,
     ExperienciaComponent,
